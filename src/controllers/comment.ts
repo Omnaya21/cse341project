3cb03c3269c74807c5d30515f4a8ad54df8e8eb4
Kendra Bryant <kbryant@efilecabinet.com>
2023-07-08 09:32:32 -0600

    Added swagger comments

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -8,24 +8,51 @@ import {
 import { Request, Response } from 'express';
 
 export const getCommentById = async (req: Request, res: Response) => {
+  /*
+    #swagger.responses[200] = {
+      description: 'Get a comment by its id.',
+      schema: { $ref: '#/definitions/Comment'}
+    }
+  */
   const commentId = req.params.commentId;
   const comment = await dbGetComment(commentId);
   res.status(200).send(comment);
 };
 
 export const insertComment = async (req: Request, res: Response) => {
+  /*
+    #swagger.requestBody  = {
+      description: 'Create a new comment',
+      in: 'body',
+      required: true,
+      schema: { $ref: '#/definitions/Comment'}
+    }
+  */
   const comment = await dbInsertComment(req.body);
   res.status(201).send(comment);
 };
 
 export const updateComment = async (req: Request, res: Response) => {
+  /*
+    #swagger.requestBody  = {
+      description: 'Update a comment',
+      in: 'body',
+      required: true,
+      schema: { $ref: '#/definitions/Comment'}
+    }
+  */
   const commentId = req.params.commentId;
   const comment = await dbUpdateComment(commentId, req.body);
   res.status(202).send(comment);
 };
 
 export const deleteComment = async (req: Request, res: Response) => {
+  /*
+    #swagger.responses[204] = {
+      description: 'Comment successfully deleted.',
+    }
+  */
   const commentId = req.params.commentId;
   await dbDeleteComment(commentId);
-  res.status(200).send();
+  res.status(204).send();
 };
--- src/controllers/tag.ts
+++ src/controllers/tag.ts
@@ -8,24 +8,51 @@ import {
 import { Request, Response } from 'express';
 
 export const getTagById = async (req: Request, res: Response) => {
+  /*
+    #swagger.responses[200] = {
+      description: 'Get a tag by its id.',
+      schema: { $ref: '#/definitions/Tag'}
+    }
+  */
   const tagId = req.params.tagId;
   const tag = await dbGetTag(tagId);
   res.status(200).send(tag);
 };
 
 export const insertTag = async (req: Request, res: Response) => {
+  /*
+    #swagger.requestBody  = {
+      description: 'Create a new tag',
+      in: 'body',
+      required: true,
+      schema: { $ref: '#/definitions/Tag'}
+    }
+  */
   const tag = await dbInsertTag(req.body);
   res.status(201).send(tag);
 };
 
 export const updateTag = async (req: Request, res: Response) => {
+  /*
+    #swagger.requestBody  = {
+      description: 'Update a tag',
+      in: 'body',
+      required: true,
+      schema: { $ref: '#/definitions/Tag'}
+    }
+  */
   const tagId = req.params.tagId;
   const tag = await dbUpdateTag(tagId, req.body);
   res.status(202).send(tag);
 };
 
 export const deleteTag = async (req: Request, res: Response) => {
+  /*
+    #swagger.responses[204] = {
+      description: 'Tag successfully deleted.',
+    }
+  */
   const tagId = req.params.tagId;
   await dbDeleteTag(tagId);
-  res.status(200).send();
+  res.status(204).send();
 };
